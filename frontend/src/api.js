71f99c56b49f67374b63ef0accb3d17b82c19eca
Vikas Kashyap <126238976+vikaskashyap7@users.noreply.github.com>
2024-11-24 16:14:42 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
+const API = axios.create({ baseURL: 'https://rbcui.onrender.com/api' });
 
 export const fetchUsers = () => API.get('/users');
 export const createUser = (data) => API.post('/users', data);
